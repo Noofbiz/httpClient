bd134a28afe62d6be1dbd1b5988a1906fa8e00b6
Jerry Caligiure <caligiure.ja@gmail.com>
2018-02-27 16:33:38 +0000

    added headers for both response and request and ability to read body and properly exit

--- client.go
+++ client.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"strconv"
+	"strings"
 )
 
 // PrintBody sends a generic GET request to the server at url and prints
@@ -14,22 +16,56 @@ func PrintBody(url string) error {
 		return err
 	}
 
+	header := "GET " + u.path + " HTTP/1.1\r\n"
+	header += "Host: " + u.hostname + "\r\n"
+	header += "User-Agent: Noofbizzle\r\n"
+	header += "Accept: text/html\r\n"
+	header += "Accept-Language: en-us\r\n"
+	header += "Accept-Encoding: gzip,deflate\r\n"
+	header += "Accept-Charset: ISO-8859-1,utf-8\r\n\r\n"
+
 	conn, err := net.Dial("tcp", "["+u.hostname+"]:"+u.port)
 	if err != nil {
 		return err
 	}
+	defer conn.Close()
 
-	_, err = conn.Write([]byte("GET / HTTP/1.0\r\n\r\n"))
+	_, err = conn.Write([]byte(header))
 	if err != nil {
 		return err
 	}
 
-	resp, err := bufio.NewReader(conn).ReadString('\n')
+	resp := bufio.NewReader(conn)
+	respHeader := make(map[string]string)
+	// get the header
+	for {
+		line, err := resp.ReadString('\n')
+		if err != nil {
+			return err
+		}
+		if line == "\r\n" {
+			break //header ends with an empty line
+		}
+		if strings.HasPrefix(line, "HTTP/") {
+			respHeader["first"] = line
+			continue
+		}
+		nv := strings.Split(line, ": ")
+		respHeader[nv[0]] = strings.TrimSuffix(nv[1], "\r\n")
+	}
+
+	n, err := strconv.Atoi(respHeader["Content-Length"])
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(resp)
+	buf := make([]byte, n)
+	_, err = resp.Read(buf)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%s", buf)
 
 	return nil
 }
